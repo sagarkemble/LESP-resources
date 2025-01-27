173e49988ab5069bf9bc785e3830480fbb7278ed
Sagar Kemble <sagarkemble.sasa@gmail.com>
2025-01-27 23:58:58 +0530

    fixed nav

--- build/script.js
+++ build/script.js
@@ -1,3 +1,34 @@
+const navLinks = document.querySelector(".links-container");
+const navMenuIcon = document.querySelector(".menu-button-wrapper");
+const subLink = document.querySelector(".sublink");
+const subLinkToggleButton = document.querySelector(".sublink-toggle-button");
+
+navMenuIcon.addEventListener("click", () => {
+  if (navLinks.style.maxHeight) {
+    navLinks.style.maxHeight = null;
+
+    navLinks.style.opacity = "0";
+  } else {
+    navLinks.style.maxHeight = "100vh";
+    navLinks.style.opacity = "100";
+  }
+});
+
+subLinkToggleButton.addEventListener("click", () => {
+  if (window.innerWidth < 768) {
+    if (subLink.style.maxHeight) {
+      subLink.style.maxHeight = null;
+      subLink.style.paddingTop = "0";
+      subLink.style.opacity = "0";
+    } else {
+      subLink.style.maxHeight = subLink.scrollHeight + "px";
+      subLink.style.paddingTop = "1.5rem";
+      subLink.style.opacity = "100";
+    }
+  } else {
+  }
+});
+
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
