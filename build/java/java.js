73c92d1373418c997f5e7b762f5e5162c7cb0ae4
sagar kemble <sagarkemble.sasa@gmail.com>
2025-02-16 01:33:35 +0530

    java all resources shift to js

--- build/java/java.js
+++ build/java/java.js
@@ -51,6 +51,52 @@ const content = {
       link: "https://drive.google.com/file/d/18TcOloCDdRBjh8croc1KZ2QmqIZHI52a/view?usp=drivesdk",
     },
   },
+  other_resources_Obj: {
+    qb: {
+      name: "QB UT-1",
+      link: "https://drive.google.com/file/d/1AOcT-RuSXDwNjtf3ahT88lTQIV1oaPX3/view?usp=drivesdk",
+    },
+    syllabus: {
+      name: "Syllabus",
+      link: "https://drive.google.com/file/d/1n1GKosTv0jxSdrgv0d7X5YyOi4qHRQKw/view?usp=drive_link",
+    },
+
+    EX_3_OP: {
+      name: "EX-3 O/P",
+      link: "https://drive.google.com/file/d/13BbCSxkLQJ3-U3bMKXgGBK0KlZD6Xkg4/view?usp=drivesdk",
+    },
+
+    EX_4_OP: {
+      name: "EX-4 O/P",
+      link: "https://drive.google.com/file/d/13FedfSSQ3eJ7xQ4GuNFBUq2lyTcFW_c8/view?usp=drivesdk",
+    },
+    EX_5_OP: {
+      name: "EX-5 O/P",
+      link: "https://drive.google.com/file/d/130e6rRKcU-Pi4QIYMTlr7PBscWr9BFMg/view?usp=drivesdk",
+    },
+    EX_7_OP: {
+      name: "EX-7 O/P",
+      link: "https://drive.google.com/file/d/134XSaWIiN8-Ke32fQVWsiFUGhibH26Ui/view?usp=drivesdk",
+    },
+    EX_8_OP: {
+      name: "EX-8 O/P",
+      link: "https://drive.google.com/file/d/13-lawS6Cz6MVsLcvVTRCHRjZv525LLGp/view?usp=drivesdk",
+    },
+  },
+  notes_obj: {
+    unit_1: {
+      name: "Unit-1",
+      link: "https://drive.google.com/file/d/1cxVy7je24dOFORZq5ka3VijzrN7h2FvO/view?usp=sharing",
+    },
+    unit_2: {
+      name: "Unit-2",
+      link: "https://drive.google.com/file/d/1r1fWs4QGMn8_kewJzIZ1qj0yTAbNOOCD/view?usp=sharing",
+    },
+    unit_3: {
+      name: "Unit-3",
+      link: "https://drive.google.com/file/d/1t9upXsQtHy72YYvrQQnzMp7SnecGlkOB/view?usp=sharing",
+    },
+  },
 };
 
 const assignment_container = document.querySelector(
@@ -65,6 +111,9 @@ const notes_container = document.querySelector(
 );
 const other_resources_container = document.querySelector(
   ".other-resources-cards-container",
+);
+const Notes_resources_container = document.querySelector(
+  ".Notes-resources-cards-container",
 );
 
 function createCard(obj, container) {
@@ -96,3 +145,5 @@ createCard(
   content.manual_questionandanswer_obj,
   manual_questionandanswer_container,
 );
+createCard(content.other_resources_Obj, other_resources_container);
+createCard(content.notes_obj, Notes_resources_container);
