eb0d0b6f1ab8f7d4d094a3afebfbc94700deb729
Sagar Kemble <sagarkemble.sasa@gmail.com>
2025-02-16 02:34:40 +0530

    shifted dcn code in folder

--- build/dcn/dcn.js
+++ build/dcn/dcn.js
@@ -2,37 +2,130 @@ const content = {
   assignment_Obj: {
     assignemnt1: {
       name: "Assignment-1",
-      link: "https://drive.google.com/file/d/1WdPYQMr2wvV9CpT-dexxeZe9BILX0GJ1/view?usp=drive_link",
+      link: "https://drive.google.com/file/d/1-4Jh8QLm5PmCkv9eG8FLC31hQuQBOLnT/view?usp=drivesdk",
     },
     assignemnt2: {
       name: "Assignment-2",
-      link: "https://drive.google.com/file/d/14NTu0mNJWAAi438wRlFAJaQsC9WaX4f3/view?usp=drivesdk",
+      link: "https://drive.google.com/file/d/11eZVAujt3um-qreLZx8cq9JE7TFmXxJ9/view?usp=drivesdk",
     },
     assignemnt3: {
       name: "Assignment-3",
-      link: "/build/notuploaded.html",
+      link: "../notuploaded.html",
     },
     assignemnt4: {
       name: "Assignment-4",
-      link: "/build/notuploaded.html",
+      link: "../notuploaded.html",
     },
     assignemnt5: {
       name: "Assignment-5",
-      link: "/build/notuploaded.html",
+      link: "../notuploaded.html",
     },
     assignemnt6: {
       name: "Assignment-6",
-      link: "/build/notuploaded.html",
+      link: "../notuploaded.html",
     },
   },
-  manual_obj: {
+  class_notes_obj: {
+    unit1: {
+      name: "Unit-1",
+      link: "https://drive.google.com/file/d/16xIfY1s-ROCeDd6Kles_GbKF9mdYC6jY/view?usp=drivesdk",
+    },
+    unit2: {
+      name: "Unit-2",
+      link: "https://drive.google.com/file/d/170nl0aRgmmbnUysrlWhus1iZ4W1wCj7n/view?usp=drivesdk",
+    },
+  },
+  notes_obj: {
+    unit1: {
+      name: "Unit-1",
+      link: "https://drive.google.com/file/d/12D6f3bm5izYB5d0ztpsv1QJs-dmKWFVc/view?usp=drivesdk",
+    },
+    unit2: {
+      name: "Unit-2",
+      link: "https://drive.google.com/file/d/12E_nENUww9uGjjH1EHbOJ_nxp9QZKsFW/view?usp=drivesdk",
+    },
+    unit3: {
+      name: "Unit-3",
+      link: "https://drive.google.com/file/d/12IMPrVxN_IduIY7nYDrua5RKYy1qEGgI/view?usp=drivesdk",
+    },
+  },
+  manual_questionandanswer_obj: {
+    exp1: {
+      name: "Experiment-1",
+      link: "https://drive.google.com/file/d/1CszJ_NMP_ebQ44MntBTwl_dbkDtfr1JO/view?usp=drivesdk",
+    },
+    exp2: {
+      name: "Experiment-2",
+      link: "https://drive.google.com/file/d/1Cy2ulBt7_yRyv7qflm7M30YW97b12fRp/view?usp=drivesdk",
+    },
+    exp3: {
+      name: "Experiment-3",
+      link: "https://drive.google.com/file/d/1Cu1uFx-KoGw8L_eRwCzkdi4k3DghrjBY/view?usp=drivesdk",
+    },
+    exp4: {
+      name: "Experiment-4",
+      link: "https://drive.google.com/file/d/1CvHxHo7SqLLNCw8YAgisXXwELYzeo44K/view?usp=drivesdk",
+    },
+    exp5: {
+      name: "Experiment-5",
+      link: "https://drive.google.com/file/d/1CzqCzhURCUU6huZUeq007Za9T8B4o3ci/view?usp=drivesdk",
+    },
+  },
+  other_resources_Obj: {
+    qb: {
+      name: "QB UT-1",
+      link: "https://drive.google.com/file/d/163Xzoq9CbS2uG_xz8DGvaLnn3OqeSx6j/view?usp=drivesdk",
+    },
+    syllabus: {
+      name: "Syllabus",
+      link: "https://drive.google.com/file/d/1IElLQ5rcPuQWjtTD-e5tNcNP7Ndm_iPi/view?usp=drive_link",
+    },
+
+    PYQ1: {
+      name: "PYQ-2019",
+      link: "https://drive.google.com/file/d/1GouoV-a2va5wYKaaTsWiZvHzd0ypeIbE/view?usp=drive_link",
+    },
+    PYQ2: {
+      name: "PYQ-2019-2",
+      link: "https://drive.google.com/file/d/16xN5q5kJ9lNyOfhLPFPlchjxtYzBhOiz/view?usp=drive_link",
+    },
+    PYQ3: {
+      name: "PYQ-2022",
+      link: "https://drive.google.com/file/d/1w0g_c4BY3zq01SabYY0TnTXI_P90gCUz/view?usp=drive_link",
+    },
+  },
+  manual_Obj: {
+    manual0: {
+      name: "Index",
+      link: "https://drive.google.com/file/d/1-9UD9qJIPT9xI5sORZTVr3ASXdUqh0NB/view?usp=drive_link",
+    },
     manual1: {
-      name: "Manual-1",
-      link: "https://drive.google.com/file/d/1WdPYQMr2wvV9CpT-dexxeZe9BILX0GJ1/view?usp=drive_link",
+      name: "Manual 1-5",
+      link: "https://drive.google.com/file/d/1-UCeFCMhyY0EeXVlHxncAURx3tiuxgFx/view?usp=drive_link",
     },
     manual2: {
-      name: "Manual-2",
-      link: "https://drive.google.com/file/d/1WdPYQMr2wvV9CpT-dexxeZe9BILX0GJ1/view?usp=drive_link",
+      name: "Manual 6-10",
+      link: "https://drive.google.com/file/d/1-S9XeEWCvyFxTJy3a20NtNtWWl5XBb4M/view?usp=drive_link",
+    },
+    manual3: {
+      name: "Manual 11-15",
+      link: "https://drive.google.com/file/d/1-S4bnE3AYYFE76Oc4iZTRqLVCsUvyF53/view?usp=drive_link",
+    },
+    manual4: {
+      name: "Manual 16-20",
+      link: "https://drive.google.com/file/d/1-RoQe0A0q0la5fd-JI4J161f_jMWaWdK/view?usp=drive_link",
+    },
+    manual5: {
+      name: "Manual 21-25",
+      link: "https://drive.google.com/file/d/1-ND05yNHbQ23b0MQP6p2iPiccNalRENE/view?usp=drive_link",
+    },
+    manual6: {
+      name: "Manual 26-28",
+      link: "https://drive.google.com/file/d/1-KQAp3fjTBQi8Q1ye_-qcIZfigA5WZNu/view?usp=drive_link",
+    },
+    manual7: {
+      name: "Full Manual",
+      link: "https://drive.google.com/file/d/14v8vtY1-8ZgsUxvZRCnRNa9IfIecU4An/view?usp=drive_link",
     },
   },
 };
@@ -40,8 +133,17 @@ const content = {
 const assignment_container = document.querySelector(
   ".assignment-cards-container",
 );
-const manual_container = document.querySelector(
+const class_notes_container = document.querySelector(
+  ".class-notes-cards-container",
+);
+const notes_container = document.querySelector(".notes-cards-container");
+const manual_questionandanswer_container = document.querySelector(
   ".manaul-question-answer-cards-container",
+);
+const manual_container = document.querySelector(".manual-cards-container");
+
+const other_resources_container = document.querySelector(
+  ".other-resources-cards-container",
 );
 
 function createCard(obj, container) {
@@ -69,4 +171,11 @@ function createCard(obj, container) {
 
 // Load Assignments
 createCard(content.assignment_Obj, assignment_container);
-createCard(content.manual_obj, manual_container);
+createCard(content.class_notes_obj, class_notes_container);
+createCard(content.other_resources_Obj, other_resources_container);
+createCard(content.notes_obj, notes_container);
+createCard(content.manual_Obj, manual_container);
+createCard(
+  content.manual_questionandanswer_obj,
+  manual_questionandanswer_container,
+);
