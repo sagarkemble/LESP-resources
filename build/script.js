3283066e48a31c8dc236e86df380b40d95b108fb
Rohan Korake <rohannkorake@gmail.com>
2025-03-23 08:02:14 +0530

    JPR exp 27 O/P added

--- build/script.js
+++ build/script.js
@@ -37,7 +37,7 @@ subLinkToggleButton.addEventListener("click", () => {
 
 // popup start
 document.addEventListener("DOMContentLoaded", () => {
-  let popupversion = "2.20";
+  let popupversion = "2.21";
   const localStorageData = localStorage.getItem("updatepopupShown");
   console.log(localStorageData);
 
