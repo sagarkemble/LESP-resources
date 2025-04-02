70e062154ce4c3a89ba1f793da85e0a328f1499f
Varad Harlikar <harlikarvarad@gmail.com>
2025-04-02 18:51:18 +0530

    minor changes

--- build/script.js
+++ build/script.js
@@ -37,7 +37,7 @@ subLinkToggleButton.addEventListener("click", () => {
 
 // popup start
 document.addEventListener("DOMContentLoaded", () => {
-  let popupversion = "2.29";
+  let popupversion = "2.30";
   const localStorageData = localStorage.getItem("updatepopupShown");
   console.log(localStorageData);
 
