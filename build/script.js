64e280577ab7f8b721a11dab14935025635c195d
Rohan Korake <rohannkorake@gmail.com>
2025-04-06 19:40:24 +0530

    Notice added

--- build/script.js
+++ build/script.js
@@ -37,7 +37,7 @@ subLinkToggleButton.addEventListener("click", () => {
 
 // popup start
 document.addEventListener("DOMContentLoaded", () => {
-  let popupversion = "2.34";
+  let popupversion = "2.35";
   const localStorageData = localStorage.getItem("updatepopupShown");
   console.log(localStorageData);
 
