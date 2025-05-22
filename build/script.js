0179712ceb18cd15ee62c59c3458da22d0b10c5e
Varad Harlikar <harlikarvarad@gmail.com>
2025-05-22 17:15:55 +0530

    ..

--- build/script.js
+++ build/script.js
@@ -37,7 +37,7 @@ subLinkToggleButton.addEventListener("click", () => {
 
 // popup start
 document.addEventListener("DOMContentLoaded", () => {
-  let popupversion = "2.48";
+  let popupversion = "2.49";
   const localStorageData = localStorage.getItem("updatepopupShown");
   console.log(localStorageData);
 
